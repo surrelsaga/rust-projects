7e19274fe33fee67979889ddc1eebe90b4c4ce2f
Daniel Nguyen <233656241+DucAnh0106@users.noreply.github.com>
2026-09-15 18:46:53 +0800

    chap-01: implement Flee move for players only: 50% of dodging the monster attack

--- 01-dungeon-crawler/src/main.rs
+++ 01-dungeon-crawler/src/main.rs
@@ -56,22 +56,53 @@ impl Monster {
     }
 }
 
+fn parse_player_move(playerMove: &str) -> Option<Action> {
+    match playerMove {
+        "attack" => Some(Action::Attack),
+        "defend" => Some(Action::Defend),
+        "flee" => Some(Action::Flee),
+        _ => None,
+    }
+}
+
 // game logic (flee is not implemented yet since it's not straightforward)
-fn playGame(player: &mut Player, monster: &mut Monster, playerMove: &str, monsterMove: Action) {
+fn playGame(player: &mut Player, monster: &mut Monster, playerMove: Action, monsterMove: Action) {
     // one round only
+
+    // Only player can flee (dodge)
+
+    // if let is just match but only have 1 case and ignore the other cases
+    if let Action::Flee = playerMove {
+        // generate true, fasle randomly (coin flip)
+
+        // if true then can dodge
+        if rand::thread_rng().gen_bool(0.5) {
+            println!("You dodged its attack.");
+        }
+
+        // otherwise, flee does not have any use
+        // so if monster attacks, player will take damage
+        println!("You failed to dodge.");
+        if let Action::Attack = monsterMove {
+            monster.attack(player);
+        }
+    }
+
+
+    // handle only Attack and Defend here
     match (playerMove, monsterMove) {
-        ("attack", Action::Attack) => {
+        (Action::Attack, Action::Attack) => {
             // both attack
             player.attack(monster);
             monster.attack(player);
         },
 
-        ("attack", Action::Defend) => {
+        (Action::Attack, Action::Defend) => {
             // monster takes half the damage
             player.attack_against_defend(monster);
         },
 
-        ("defend", Action::Attack) => {
+        (Action::Defend, Action::Attack) => {
             // player takes half the damage
             monster.attack_against_defend(player);
         },
@@ -93,7 +124,7 @@ fn main() {
     };
 
     // TAKE USER INPUT FOR PLAYER'S MOVE
-    
+
     println!("Enter your move (attack/defend/flee): ");
 
     let mut playerMove = String::new();
@@ -106,28 +137,28 @@ fn main() {
 
     // shadow it without a .trim() to convert to a &str 
     // so it can accept literal string
-    let playerMove: &str = playerMove.trim();
 
-
+    // then convert to an Action (w/o expect, it would be type Option<Action> not Action)
+    let playerMove: Action = parse_player_move(playerMove.trim())
+                                .expect("invalid move, type attack/defend/flee");
 
     // RANDOMIZE MONSTER'S MOVE
 
-    // gen number from 0 to 1 ( should be to 2, but flee is not implemented yet )
+    // gen number from 0 to 1
     let randomNum = rand::thread_rng().gen_range(0..=1);
 
-    // monster Move
-    let monsterMove: Action = match randomNum {
-        0 => Action::Attack,
-        1 => Action::Defend,
-        // 2 => Action::Flee, (implement later)
-        _ => unreachable!("monster move must be 0 or 1"),
+    // monster Move (monster dont know how to dodge)
+    let monsterMove: Action = if randomNum == 0 {
+        Action::Attack
+    } else {
+        Action::Defend
     };
 
     println!("{:?}", monsterMove);
 
 
     // Test Playing 1 round (play manually)
-    playGame(&mut player, &mut monster, "attack", Action::Defend);
+    playGame(&mut player, &mut monster, playerMove, Action::Defend);
 
     println!("Monster HP after one round: {}", monster.hp);
 }
